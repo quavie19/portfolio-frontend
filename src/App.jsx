c88f2c2d058476aa353a9d34532d69e91dd6eb9f
Jaquavia <97323413+quavie19@users.noreply.github.com>
2025-05-11 10:02:43 -0400

    react routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+//pages
+import Homepage from './pages/Homepage';
+import AboutMe from './pages/AboutMe';
+import ContactMe from './pages/ContactMe';
+import NewPost from './pages/NewPost';
+import BlogPost from './pages/BlogPost';
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='about' element={<AboutMe />} />
+        <Route path='contact' element={<ContactMe />} />
+        <Route path='new' element={<NewPost />} />
+        <Route path='blog' element={<BlogPost />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/ContactMe.jsx
@@ -0,0 +1,5 @@
+const ContactMe = () => {
+  return <></>;
+};
+
+export default ContactMe;
