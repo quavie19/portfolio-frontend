b26f5c68656b47890ddfb0509bffddcdd7900d2a
Jaquavia <97323413+quavie19@users.noreply.github.com>
2025-05-12 12:40:14 -0400

    contact form

--- src/pages/ContactMe.jsx
+++ src/pages/ContactMe.jsx
@@ -1,5 +1,26 @@
 const ContactMe = () => {
-  return <></>;
+  return (
+    <div className='contact'>
+      <h1>Contact Me</h1>
+      <form action='#' method='post'>
+        <div className='name'>
+          <label for='fname'>Name</label>
+          <input type='text' id='fname' name='firstname' />
+        </div>
+        <div className='email'>
+          <label for='email'>Email</label>
+          <input type='email' id='lname' name='email' />
+        </div>
+        <div className='message'>
+          <label for='message'>Message</label>
+          <input type='text' id='message' name='message' />
+        </div>
+        <div className='submit'>
+          <input type='submit' value='Submit' />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ContactMe;
