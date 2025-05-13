3b833fffd3dc4ef5c335a5ea695822dbd9829548
Jaquavia <97323413+quavie19@users.noreply.github.com>
2025-05-13 17:49:23 -0400

    about me section

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,5 +1,31 @@
+import LOGO from '../img/LOGO.jpg';
+
 const AboutMe = () => {
-  return <></>;
+  return (
+    <div id='about'>
+      <div className='header-content'>
+        <div className='tite'>
+          <h1>Jaquavia</h1>
+          <h2>Software Engineer</h2>
+        </div>
+        <div className='profile-pic'>
+          <img
+            src={LOGO}
+            alt='profile-pic'
+            style={{ height: '200px', borderRadius: '50%' }}
+          />
+        </div>
+      </div>
+      <div className='bio'>
+        <p>
+          I’m a full-stack developer who loves building things that work and
+          look good. Here, I break down front-end and back-end concepts in a way
+          that’s fun, real, and actually makes sense
+        </p>
+      </div>
+      <div></div>
+    </div>
+  );
 };
 
 export default AboutMe;
